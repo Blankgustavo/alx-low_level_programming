e27f5c48b3f7838cd245ac8069572d26b31bc49e
gustavo <gustokkaunda@gmail.com>
2023-03-14 12:33:14 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,9 +7,9 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-char *str_concat(chat *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
-	char *conct
+	char *conct;
 	int i, ci;
 
 	if (s1 == NULL)
@@ -17,7 +17,7 @@ char *str_concat(chat *s1, char *s2)
 	i = ci = 0;
 	while (s1[i] != '\0')
 		ci++;
-	concat = malloc(sizeof(char) * (i + ci + 1));
+	conct = malloc(sizeof(char) * (i + ci + 1));
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
