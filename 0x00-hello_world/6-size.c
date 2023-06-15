354b24356bbed14d9d8ce22b973d3205270b6d04
blankgustavo <gustokkaunda@gmail.com>
2023-06-15 11:04:45 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /*
- *  *
- *   *main-entery
- *    *retutn:always 0
- *     *
- *      */
+ *main-entery
+ *retutn:always 0
+ */
+
 int main(void)
 {
-	printf("size of int:%d bytes", (unsigned char)sizeof(int));
-	printf("size of char:%d bytes", (unsigned char)sizeof(char));
-	printf("size of long int:%d bytes", (unsigned char)sizeof(long int));
-	printf("size of long long int:%d bytes", (unsigned char)sizeof(long long int));
-	printf("size of float:%d bytes", (unsigned char)sizeof(float));
+	printf("size of int:%d byte(s)", sizeof(int));
+	printf("size of char:%d byte(s)", sizeof(char));
+	printf("size of long int:%d byte(s)", sizeof(long int));
+	printf("size of long long int:%d byte(s)", sizeof(long long int));
+	printf("size of float:%d byte(s)", sizeof(float));
 	return (0);
 }
